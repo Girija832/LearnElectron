896a7d00b8c0a5ebc50f5a12e81d565849a0c473
Girija Shankar Sahoo <girija.sahoo@mobtexting.com>
2018-01-06 11:25:25 +0530

    Print hello world

---
+++ main.js
@@ -0,0 +1,9 @@
+const {app,BrowserWindow} = require('electron');
+let mainWindow;
+app.on('ready',()=> {
+	mainWindow = new BrowserWindow({
+		height : 600,
+		width : 800
+	});
+	mainWindow.loadURL('File://' +__dirname+ '/index.html');
+});
